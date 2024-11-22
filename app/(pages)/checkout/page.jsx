f2fbf0675a0e9193bd1aa5d69a3fb0848ab34d90
asib27 <asib.rahman1927@gmail.com>
2024-11-23 05:55:03 +0600

    add checkbox

--- app/(pages)/checkout/page.jsx
+++ app/(pages)/checkout/page.jsx
@@ -1,4 +1,11 @@
+'use client'
+
+import StarRating from "@/components/star-rating";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ShoppingBag } from "lucide-react";
+import { useState } from "react";
+import { useImmer } from "use-immer";
 
 let sidebarMenus = [
   "Rating", "Price", "Color", "Brand", "Size"
@@ -22,30 +29,86 @@ let sidebarMenuDict = {
   }
 }
 
+const ShowRating = ({rating_dict, filterState, updateFilterState}) => {
+  return (
+    <div className="">
+      {
+        Array.from({ length: 5 }, (_, index) => {
+          return (
+            <AccordionContent className="p-0" key={index}>
+              <div className="flex gap-1 py-1 items-center">
+                <Checkbox 
+                checked={filterState["Rating"][5 - index]}
+                className='h-4 w-4' 
+                onCheckedChange={(e) => updateFilterState(st => {st['Rating'][5-index] = e})} />
+                <StarRating stars={5 - index} total={5}/>
+                <p>
+                  {`(${rating_dict[5 - index] ? rating_dict[5 - index]: 0})`}
+                </p>
+              </div>
+            </AccordionContent>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const InnerAccordion = ({menu, filterState, updateFilterState}) => {
+  if (menu === "Rating") {
+    return (
+      <ShowRating rating_dict={sidebarMenuDict[menu]} filterState={filterState} updateFilterState={updateFilterState}/>
+    )
+  }
+  else {
+    return (
+      <div>
+      {       
+        Object.keys(sidebarMenuDict[menu]).map((menuItem, index) => {
+          return (
+              <AccordionContent key={index} className='p-0'>
+                {`${menuItem} - ${sidebarMenuDict[menu][menuItem]}`}
+              </AccordionContent>
+          );
+        })
+      }
+      </div>
+      
+    )
+  }
+}
+
 export default function Checkout() {
+  const [filterState, updateFilterState] = useImmer({
+    'Rating': {
+      "5": true, "4": true, "3": true, "2" : true, "1": true
+    },
+    'Price': {
+      "< 2000": true, "2000-3000": true, "3000-4000": true, "4000-5000": true, " > 5000": true
+    },
+    'Color': {
+      'White': true, 'Black': true, 'Blue': true, 'Red': true, 'Green': true
+    },
+    'Brand': {
+      'Nike': true, 'Adidas': true, 'Puma': true
+    },
+    'Size': {
+      '38': true, '39': true, '40': true, '41': true, '42': true, '43': true, '44': true
+    }
+    })
+
+
   return ( 
-  <div className="w-full flex h-svh max-h-svh">
-    <div className="h-full flex-[0.4] rounded-2xl bg-slate-200 dark:bg-slate-900">
+  <div className="w-full flex max-h-svh">
+    <div className="flex-[0.25] rounded-2xl bg-slate-200 dark:bg-slate-900">
     {
       Object.keys(sidebarMenuDict).map((menu, index) => {
         return (
-          <Accordion type="multiple"  key={index}>
+          <Accordion type="multiple" className=""  key={index}>
             <div className="px-5 pt-1">
               <AccordionItem value={menu}>
-              <AccordionTrigger>{menu}</AccordionTrigger>
-              {
-                Object.keys(sidebarMenuDict[menu]).map((menuItem, index) => {
-                  // console.log(sidebarMenuDict[menu], menuItem)
-                  return (
-                    
-                      <AccordionContent>
-                        {`${menuItem} - ${sidebarMenuDict[menu][menuItem]}`}
-                      </AccordionContent>
-                    
-                  );
-                })
-                
-              }
+              <AccordionTrigger className='py-2 text-base'>{menu}</AccordionTrigger>
+              <InnerAccordion menu={menu} filterState={filterState} updateFilterState={updateFilterState}/>
               </AccordionItem>
             </div>
           </Accordion>
@@ -57,7 +120,7 @@ export default function Checkout() {
     </div>
 
     <div className="h-full flex-1">
-      Main
+      Menu
     </div>
   </div>
   )
