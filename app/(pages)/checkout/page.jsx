84f695656584c64b8941c6b4db6e39f026b387e9
asib27 <asib.rahman1927@gmail.com>
2024-11-23 06:23:23 +0600

    add side filter

--- app/(pages)/checkout/page.jsx
+++ app/(pages)/checkout/page.jsx
@@ -32,11 +32,11 @@ let sidebarMenuDict = {
 const ShowRating = ({rating_dict, filterState, updateFilterState}) => {
   return (
     <div className="">
+      <AccordionContent className="p-0">
       {
         Array.from({ length: 5 }, (_, index) => {
           return (
-            <AccordionContent className="p-0" key={index}>
-              <div className="flex gap-1 py-1 items-center">
+              <div key={index} className="flex gap-1 py-1 items-center">
                 <Checkbox 
                 checked={filterState["Rating"][5 - index]}
                 onCheckedChange={(e) => updateFilterState(st => {st['Rating'][5-index] = e})} />
@@ -45,10 +45,10 @@ const ShowRating = ({rating_dict, filterState, updateFilterState}) => {
                   {`(${rating_dict[5 - index] ? rating_dict[5 - index]: 0})`}
                 </p>
               </div>
-            </AccordionContent>
           )
         })
       }
+      </AccordionContent>
     </div>
   )
 }
